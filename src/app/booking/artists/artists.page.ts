a545b16ee358a11b2deb40e4864dd465cac8f71e
mayora95 <32702520+mayora95@users.noreply.github.com>
2021-10-01 18:24:48 +0200

    display value from firestore working

--- src/app/booking/artists/artists.page.ts
+++ src/app/booking/artists/artists.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { Artist } from 'src/app/models/artist';
+import { ArtistService } from 'src/app/services/artists/artist.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-artists',
@@ -6,10 +14,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artists.page.scss'],
 })
 export class ArtistsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  private itemsCollection: AngularFirestoreCollection<Artist>;
+  private items: Observable<Artist[]>;
+  constructor(
+    private artistServe: ArtistService,
+    private afs: AngularFirestore
+  ) {
+    this.itemsCollection = afs.collection<Artist>('artists');
+    this.items = this.itemsCollection.valueChanges();
   }
 
+  ngOnInit() {
+    // console.log(this.artistServe.getArtist2());
+  }
 }
